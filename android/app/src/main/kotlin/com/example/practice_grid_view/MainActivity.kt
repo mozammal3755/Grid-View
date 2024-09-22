fedad4ae4dc9ab0ae768149c3d0e26f7c7da1f2d
unknown <imonbdcallingit@gmail.com>
2024-09-22 11:41:25 +0600

    first commite

---
+++ android/app/src/main/kotlin/com/example/practice_grid_view/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.practice_grid_view
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,218 @@
+import 'dart:developer' as dev;
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MasonryPage(),
+    );
+  }
+}
+
+class MasonryPage extends StatefulWidget {
+  const MasonryPage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MasonryPage> createState() => _MasonryPageState();
+}
+
+class _MasonryPageState extends State<MasonryPage> {
+  final rnd = Random();
+  late List<int> extents;
+  int crossAxisCount = 3;
+
+  @override
+  void initState() {
+    super.initState();
+    extents = List<int>.generate(6, (int index) => rnd.nextInt(3) + 1);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: MasonryGridView.count(
+        crossAxisCount: crossAxisCount,
+        mainAxisSpacing: 4,
+        crossAxisSpacing: 4,
+        itemBuilder: (context, index) {
+          double extentValue;
+
+          // Custom pattern logic for [200, 100, 200, 200, 100, 100, 200, 100, 200, 200, 100, 100]
+          if (index % 6 == 0 ||
+              index % 6 == 2 ||
+              index % 6 == 3 ||
+              index % 6 == 6 ||
+              index % 6 == 8 ||
+              index % 6 == 9) {
+            extentValue = 200;
+          } else {
+            extentValue = 100;
+          }
+
+          return Tile(
+            index: index,
+            extent: extentValue,
+          );
+        },
+        itemCount: extents.length,
+      ),
+    );
+  }
+}
+
+const _defaultColor = Color(0xFF34568B);
+
+class AppScaffold extends StatelessWidget {
+  const AppScaffold({
+    Key? key,
+    required this.title,
+    this.topPadding = 0,
+    required this.child,
+  }) : super(key: key);
+
+  final String title;
+  final Widget child;
+  final double topPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: topPadding),
+        child: child,
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile({
+    Key? key,
+    required this.index,
+    this.extent,
+    this.backgroundColor,
+    this.bottomSpace,
+  }) : super(key: key);
+
+  final int index;
+  final double? extent;
+  final double? bottomSpace;
+  final Color? backgroundColor;
+
+  @override
+  Widget build(BuildContext context) {
+    dev.log('$index + ${extent.toString()}');
+    final child = Container(
+      color: backgroundColor ?? _defaultColor,
+      height: extent,
+      child: Center(
+        child: CircleAvatar(
+          minRadius: 20,
+          maxRadius: 20,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+          child: Text('$index', style: const TextStyle(fontSize: 20)),
+        ),
+      ),
+    );
+
+    if (bottomSpace == null) {
+      return child;
+    }
+
+    return Column(
+      children: [
+        Expanded(child: child),
+        Container(
+          height: bottomSpace,
+          color: Colors.green,
+        )
+      ],
+    );
+  }
+}
+
+class ImageTile extends StatelessWidget {
+  const ImageTile({
+    Key? key,
+    required this.index,
+    required this.width,
+    required this.height,
+  }) : super(key: key);
+
+  final int index;
+  final int width;
+  final int height;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(
+      'https://picsum.photos/$width/$height?random=$index',
+      width: width.toDouble(),
+      height: height.toDouble(),
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+class InteractiveTile extends StatefulWidget {
+  const InteractiveTile({
+    Key? key,
+    required this.index,
+    this.extent,
+    this.bottomSpace,
+  }) : super(key: key);
+
+  final int index;
+  final double? extent;
+  final double? bottomSpace;
+
+  @override
+  _InteractiveTileState createState() => _InteractiveTileState();
+}
+
+class _InteractiveTileState extends State<InteractiveTile> {
+  Color color = _defaultColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          if (color == _defaultColor) {
+            color = Colors.red;
+          } else {
+            color = _defaultColor;
+          }
+        });
+      },
+      child: Tile(
+        index: widget.index,
+        extent: widget.extent,
+        backgroundColor: color,
+        bottomSpace: widget.bottomSpace,
+      ),
+    );
+  }
+}
